59c09f044dfc77ad19a1444c34e8809223c75459
Tim Coote <tim@coote.org>
2017-07-11 22:07:58 +0100

    simplified. Removed ansible use

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,20 +1,14 @@
 #!/bin/bash -ex
 
-pwd
-ls -l
 
 on_chroot << EOF
 dpkg --print-architecture
-#echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
 apt install -y ansible
-cd /pi-gen/smart-home
-ls -l
-ansible --version
-free
-ANSIBLE_KEEP_REMOTE_FILES=1
-ansible-playbook -vvvvv -i 'localhost ansible_connection=local,' site.yml
+#ANSIBLE_KEEP_REMOTE_FILES=1
+#ansible-playbook -vvvvv -i 'localhost ansible_connection=local,' site.yml
 
 EOF
 
