d59d447fc1d53003057ef28b88ea2c04642669ac
Tim Coote <tim@coote.org>
2017-09-02 09:36:46 +0100

    reverting to upstream

--- go.sh
+++
@@ -1,7 +0,0 @@
-on_chroot << EOF
-cd pi-gen/smart-home
-ansible-playbook -vvv -i 'localhost ansible_connection=local,' site.yml
-EOF
-
-
-
--- go1.sh
+++
@@ -1,1 +0,0 @@
-docker run --privileged -ti -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 2225:22 pi-gen 
