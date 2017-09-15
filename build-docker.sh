8f96ca5b26b3c1cfb8322e0a801b83d495c6c348
Tim Coote <tim+git@coote.org>
2017-09-15 15:17:23 +0100

    separating out openzwave build

--- build-docker.sh
+++ build-docker.sh
@@ -53,7 +53,8 @@ fi
 $DOCKER build -t pi-gen .
 if [ "$CONTAINER_EXISTS" != "" ]; then
 	trap "echo 'got CTRL+C... please wait 5s'; $DOCKER stop -t 5 ${CONTAINER_NAME}_cont" SIGINT SIGTERM
-	time $DOCKER run --rm --privileged \
+       time $DOCKER run --privileged \
+#      time $DOCKER run --rm --privileged \
 		--volumes-from="${CONTAINER_NAME}" --name "${CONTAINER_NAME}_cont" \
 		-e IMG_NAME=${IMG_NAME}\
 		pi-gen \
