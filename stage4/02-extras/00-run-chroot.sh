a343cdd39994c657ba75ac52a9bc9bd0c5588283
Tim Coote <tim+git@coote.org>
2017-09-18 17:29:19 +0100

    add python_openzwave and urwid to build

--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -1,4 +1,6 @@
 pip3 install PyDispatcher==2.0.5
 pip3 install six
-pip3 install urwid==1.1.1
-pip3 install python_openzwave --install-option="--flavor=embed_shared"
+#pip3 install urwid==1.1.1
+#pip3 install python_openzwave --install-option="--flavor=embed_shared"
+pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
+pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
