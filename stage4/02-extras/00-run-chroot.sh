504d1dff77406fb0166d368b8aeebf880e058290
Andy Van Heusen <andy@pumpco.uk>
2017-12-13 15:36:16 +0000

    updated to install pika and RabbitMQ

--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -6,3 +6,5 @@ pip3 install six
 pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
 pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
 ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
+
+pip3 install pika
