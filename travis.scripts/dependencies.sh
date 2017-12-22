65a81df0bcfb53d85e84cc921a0a1b61d69449bb
Tim Coote <tim+git@coote.org>
2017-12-22 14:16:32 +0000

    install awscli in .travis.yml

--- travis.scripts/dependencies.sh
+++ travis.scripts/dependencies.sh
@@ -6,7 +6,7 @@ set -ev
 sudo apt install -y docker.io rng-tools #awscli #python3-dateutil  rake python git libselinux-python
 # increase loopbacks
 ./loops
-sudo pip install --user awscli
+# sudo pip install --user awscli
 # ARM magic
 sudo su -c 'echo -n ":arm:M::\\x7fELF\\x01\\x01\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x02\\x00\\x28\\x00:\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x00\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xfe\\xff\\xff\\xff:/usr/bin/qemu-arm-static:" > /proc/sys/fs/binfmt_misc/register  || true' # true in case the scrips has run before
 # launch the build and upload results
