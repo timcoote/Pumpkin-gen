58457c5d4af40bd451cba6e897278f3b9e32e81c
Tim Coote <tim+git@coote.org>
2018-01-04 16:04:50 +0000

    remove comment line within continuation stanza

--- build-docker1.sh
+++ build-docker1.sh
@@ -58,9 +58,7 @@ if [ "$CONTAINER_EXISTS" != "" ]; then
 #      time $DOCKER run --rm --privileged \
 #		timcoote/iotaa:latest \
        time $DOCKER run --privileged \
-# ??		--volumes-from="${CONTAINER_NAME}" --name "${CONTAINER_NAME}_cont" \
 		-e IMG_NAME=${IMG_NAME}\
-#                 localhost:5000/iotaa-pi-gen \
                 timcoote/iotaa-pi-gen
         bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
         cd /pi-gen; declare -a STAGES=(0); . ./build4.sh;
@@ -72,7 +70,6 @@ else
 	time $DOCKER run --name "${CONTAINER_NAME}" --privileged \
 		-e IMG_NAME=${IMG_NAME}\
 		"${config_file[@]}" \
-#                localhost:5000/iotaa-pi-gen \
                 timcoote/iotaa-pi-gen \
 		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
         cd /pi-gen; declare -a STAGES=(0); . ./build4.sh;
