60b6cdd531ff6d0c09184a26f945953c4adc7c4b
Tim Coote <tim+github.com@coote.org>
2018-04-17 12:55:57 +0100

    typo in adminapp version

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -4,7 +4,7 @@ ha_v=0.64
 hc_v=1.0rc3
 sens_v=0.36
 filebeat_v=0.53
-adminapp_v=_full_1.32.8
+adminapp_v=full_1.32.8
 
 # installing hubaccess .deb file and installing with apt. Based on this: http://bit.ly/2vIAffM
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/debs
