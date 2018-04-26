d98cbbf5e46070fc42007610f4bab1c1a90e76d1
Tim Coote <tim+github.com@coote.org>
2018-04-26 18:16:15 +0100

    back to s3 wheels for python-openzwave

--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -3,8 +3,8 @@ pip3 install PyDispatcher==2.0.5
 pip3 install six
 #pip3 install urwid==1.1.1
 #pip3 install python_openzwave --install-option="--flavor=embed_shared"
-#pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
-pip3 install python-openzwave==0.4.4
+pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
+# this doesn't work: pip3 install python-openzwave==0.4.4
 # overridden, anyway? pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
 # fixed in package? ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
 
