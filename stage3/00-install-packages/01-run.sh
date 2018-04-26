121dd14ecb6be9f07a34ea4abeded24b0b3bf57b
Tim Coote <tim+github.com@coote.org>
2018-04-26 15:42:01 +0100

    update adminapp to low logging level

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -4,7 +4,7 @@ ha_v=0.64
 hc_v=1.0rc3
 sens_v=0.38
 filebeat_v=0.53
-adminapp_v=full_1.32.8
+adminapp_v=full_1.34.1
 
 # installing hubaccess .deb file and installing with apt. Based on this: http://bit.ly/2vIAffM
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/debs
