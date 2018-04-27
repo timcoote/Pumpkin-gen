f6471b34ec7f50fda821ee81140853ab17c62a83
Tim Coote <tim+github.com@coote.org>
2018-04-27 12:59:19 +0100

    regress adminapp, add back symbolic link

--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -6,7 +6,7 @@ pip3 install six
 pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
 # this doesn't work: pip3 install python-openzwave==0.4.4
 # overridden, anyway? pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
-# fixed in package? ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
+ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
 
 pip3 install pika
 pip3 install yeelight
