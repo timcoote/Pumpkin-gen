379553767ad1cb52f02aeb2a880fee1bb2a4dcf6
Tim Coote <tim+github.com@coote.org>
2018-04-30 14:59:37 +0100

    updated changelog (a bit). retrying pip3 install of python-openzwave

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,18 @@ Changelog
 All notable changes to this project will be documented in this file.
 
 
-`1.0.1`_ - 2081-02-27
+`1.134`_ - 2081-04-30
+------------------------
+
+Changed
+~~~~~~~
+- Updated sensei-* to avoid stack crashing
+- Updated hubconnect to sync time before trying to push keys
+- Updated adminapp
+- Updated hub-connect to reduce logging
+
+
+`1.0.1`_ - 2018-02-27
 ------------------------
 
 Added
--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -1,10 +1,12 @@
+#!/bin/bash -ex
 pip3 install netifaces
 pip3 install PyDispatcher==2.0.5
 pip3 install six
 #pip3 install urwid==1.1.1
 #pip3 install python_openzwave --install-option="--flavor=embed_shared"
-pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
-# this doesn't work: pip3 install python-openzwave==0.4.4
+#pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
+# this doesn't work: reworking to establish why
+pip3 install python-openzwave==0.4.4
 # overridden, anyway? pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
 ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
 
