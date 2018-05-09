f6a52abbd5b0555878d6340896a1e1b27ca40988
Tim Coote <tim+github.com@coote.org>
2018-05-09 13:48:15 +0100

    merge in from pumpkins

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot/
-install -m 644 files/config.txt ${ROOTFS_DIR}/boot/
+install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+# documentation: what's this for? I added it! - mtc
 touch ${ROOTFS_DIR}/boot/ssh
--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -ex
 # the structure of build.sh allows sparation of non-chroot and chroot actiivties
-ha_v=0.63
-hc_v=0.0.2
-sens_v=0.26
-filebeat_v=0.51
+ha_v=0.64
+hc_v=1.0rc3
+sens_v=0.38
+filebeat_v=0.53
+adminapp_v=full_1.34.8
 
 # installing hubaccess .deb file and installing with apt. Based on this: http://bit.ly/2vIAffM
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/.vnc
 install -v -m 644 files/hubaccess.list ${ROOTFS_DIR}/etc/apt/sources.list.d
+install -v -m 644 files/pumpkin.conf ${ROOTFS_DIR}/etc/sysctl.d
 install -v -o 1000 -g 1000 -D files/hubaccess_${ha_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/pushkeys_${ha_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/hub-connect_${hc_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/sensei-core_${sens_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/sensei-master_${sens_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/filebeat_${filebeat_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
+install -v -o 1000 -g 1000 -D files/adminapp_${adminapp_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/gpgkeyin ${ROOTFS_DIR}/home/pi
 install -v -m 600 -o 1000 -g 1000 -D files/passwd ${ROOTFS_DIR}/home/pi/.vnc
 install -v -m 600 -D files/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
 install -v -D files/tightvnc.service ${ROOTFS_DIR}/lib/systemd/system/tightvnc.service
+install -v -o 1000 -g 1000 -D files/autostart ${ROOTFS_DIR}/home/pi/.config/lxsession/LXDE-pi/autostart
 on_chroot << EOF
 #update-alternatives --install /usr/bin/x-www-browser \
 #  x-www-browser /usr/bin/chromium-browser 86
@@ -46,6 +50,7 @@ apt update
 gpgconf --kill gpg-agent
 # --force-yes is deprecated, so removed. If / when a useful error occurs the newer --allow flag could be used
 # or move these to one of the xx-packages files
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
-apt -y install  hubaccess pushkeys hub-connect sensei-core sensei-master filebeat
+apt -y install  hubaccess pushkeys hub-connect sensei-core sensei-master filebeat nodejs adminapp
 EOF
