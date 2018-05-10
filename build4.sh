0ec95e7f4a0674e544ad7f280230db351c8b19c4
Tim Coote <tim+github.com@coote.org>
2018-05-10 18:08:09 +0100

    change to apt in packages part of build4.sh

--- build4.sh
+++ build4.sh
@@ -29,7 +29,7 @@ EOF
 			PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < ${i}-packages)"
 			if [ -n "$PACKAGES" ]; then
 				on_chroot << EOF
-apt update && apt upgrade && apt-get install -y $PACKAGES
+apt update && apt upgrade && apt install -y $PACKAGES
 EOF
 			fi
 			log "End ${SUB_STAGE_DIR}/${i}-packages"
