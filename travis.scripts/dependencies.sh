70d78d642b5c5b87a427b87a59706e25590e8a5b
Tim Coote <tim+github.com@coote.org>
2018-05-11 14:13:02 +0100

    later stage build only for travis

--- travis.scripts/dependencies.sh
+++ travis.scripts/dependencies.sh
@@ -16,9 +16,9 @@ rake
 
 touch {export-noobs,stage5}/SKIP
 
-time ./build-docker1.sh
-docker rm -v pigen_work
-#time CONTINUE=1 ./build-docker2.sh
+# comment in/out these two lines to get travis to upload an initial docker image for stage0 (also tried modifying to stage2). If left, travis will timeout, before completiion
+#time ./build-docker1.sh
+#docker rm -v pigen_work
 time ./build-docker2.sh
 
 aws s3 sync --exact-timestamps --region eu-west-2  --exclude "*" --include "*.zip" --include "*.info" deploy/ s3://pumpco
