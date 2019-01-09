d953feea36ba13b731df71c7fbbacc43a289dbba
Tim Coote <tim+github.com@coote.org>
2019-01-09 19:44:07 +0000

    use ansible launchpad for Stretch (ie Xenial). Bump python-openzwave to 0.4.18 as old version no longer available on pypi

--- stage4/02-extras/00-run-chroot.sh
+++ stage4/02-extras/00-run-chroot.sh
@@ -6,7 +6,7 @@ pip3 install six
 #pip3 install python_openzwave --install-option="--flavor=embed_shared"
 #pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html python_openzwave
 # this doesn't work: reworking to establish why
-pip3 install python-openzwave==0.4.4
+pip3 install python-openzwave==0.4.18
 # overridden, anyway? pip3 -vvvv install --use-wheel  --find-links=https://s3.eu-west-2.amazonaws.com:443/pumpco/index.html urwid
 ln -sf /usr/local/lib/python3.5/dist-packages/python_openzwave/ozw_config /usr/local/etc/openzwave
 
