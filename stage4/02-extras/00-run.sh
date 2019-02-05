074a6a2cdbd1ba05e8ff9d7bfbedb1afea5a0c36
andy monis <andy.monis@pumpco.co.uk>
2019-02-05 14:57:31 +0000

    S151#1802: Pumpkin-Gen - Updated with command to stop X Daemon from starting

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -25,3 +25,6 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.ssh"
 install -v -m 400 -o 1000 -g 1000 files/hubcentral.pub ${ROOTFS_DIR}/home/pi/.ssh/authorized_keys
+
+# AMONIS: 05/02/2019: Disable X window daemon from starting
+systemctl set-default multi-user.target
