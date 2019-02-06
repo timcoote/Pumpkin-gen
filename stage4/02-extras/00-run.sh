b07fc1db95374cafe0986b2179f41c5ac6c524e9
andy monis <andy.monis@pumpco.co.uk>
2019-02-06 16:10:51 +0000

    S151#1812: updated build

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -28,5 +28,5 @@ install -v -m 400 -o 1000 -g 1000 files/hubcentral.pub ${ROOTFS_DIR}/home/pi/.ss
 
 # AMONIS: 05/02/2019: Disable X window daemon from starting
 sudo systemctl set-default multi-user.target
-# sudo ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+sudo ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
 # sudo rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
