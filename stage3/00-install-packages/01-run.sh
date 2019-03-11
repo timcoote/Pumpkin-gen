709a7bb6a6ec45a98cb6ad058a69edb133c115e4
Andy Van Heusen <andy.van.heusen@anthropos.io>
2019-03-11 16:05:08 +0000

    added hub connect

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # the structure of build.sh allows sparation of non-chroot and chroot actiivties
 ha_v=0.65
-hc_v=1.38.rc1
+hc_v=2.0
 config_v=0.1
 sens_v=0.1
 filebeat_v=0.54
@@ -16,7 +16,7 @@ install -v -m 644 files/pumpkin.conf ${ROOTFS_DIR}/etc/sysctl.d
 install -v -o 1000 -g 1000 -D files/hubaccess_${ha_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/pushkeys_${ha_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/pumpkin-config_${config_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
-install -v -o 1000 -g 1000 -D files/hub-connect_${hc_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
+install -v -o 1000 -g 1000 -D files/hub-connect2_${hc_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/sensei2_${sens_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/filebeat_${filebeat_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
 install -v -o 1000 -g 1000 -D files/hubdaemon_${hubdaemon_v}_all.deb ${ROOTFS_DIR}/home/pi/debs
@@ -65,6 +65,6 @@ gpgconf --kill gpg-agent
 # or move these to one of the xx-packages files
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 # temp move of bcg-presenceservice to stage4 as its pre.sh runs pip3, which isn't installed, yet.
-apt -y install  hubaccess pushkeys pumpkin-config sensei2 nodejs hubdaemon filebeat
+apt -y install  hubaccess pushkeys pumpkin-config sensei2 hub-connect2 nodejs hubdaemon filebeat
 
 EOF
