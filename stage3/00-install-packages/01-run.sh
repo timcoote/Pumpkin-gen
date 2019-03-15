3a6936a091ccff122fcd6345dd4f5cb87c759c60
Andy Van Heusen <andy.vanheusen@anthropos.io>
2019-03-15 15:12:44 +0000

    move node update filebeats and sensei

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -4,7 +4,7 @@ ha_v=1.01
 hc_v=2.0
 config_v=0.1
 sens_v=0.1
-filebeat_v=0.54
+filebeat_v=0.55
 hubdaemon_v=1.54.19
 bcg_p_v=0.5
 
@@ -65,8 +65,6 @@ gpgconf --kill gpg-agent
 # or move these to one of the xx-packages files
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 # temp move of bcg-presenceservice to stage4 as its pre.sh runs pip3, which isn't installed, yet.
-apt -y install hubaccess pushkeys pumpkin-config sensei2 filebeat nodejs hubdaemon
-
-
+apt -y install hubaccess pushkeys pumpkin-config sensei2 filebeat hubdaemon
 
 EOF
