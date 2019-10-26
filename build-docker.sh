424ff3c03bc31f3307bf97e6b24d8198bf7303e8
Tim Coote <tim+github.com@coote.org>
2019-10-26 20:48:03 +0100

    take in single build from nox

--- build-docker.sh
+++ build-docker.sh
@@ -25,10 +25,13 @@ fi
 # Checkin and push. When built repeat process in reverse. 
 #
 # alternates for first and subsequent elements
-### stages="declare -a STAGES=(0)"   # this hack expands to a declaration of an array that gets fed into the build script in docker container
-### PI_GEN="pi-gen"
-stages="declare -a STAGES=(1 2 3 4 5)"
-PI_GEN="timcoote/iotaa-pi-gen-stage0:"$SPRINT""
+###stages="declare -a STAGES=(0)"   # this hack expands to a declaration of an array that gets fed into the build script in docker container
+###PI_GEN="pi-gen"
+###stages="declare -a STAGES=(1 2 3 4 5)"
+###PI_GEN="timcoote/iotaa-pi-gen-stage0:"$SPRINT""
+# temporarily do the whole lot
+stages="declare -a STAGES=(0 1 2 3 4 5)"
+PI_GEN="pi-gen"
 # used in this script, to decide on whether to push the docker image, and sent to the docker run command, too
 eval $stages
 
