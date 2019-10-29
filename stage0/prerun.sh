6c3465b37633f70bc35cd5b1e5ad63b40b1bbe2d
Tim Coote <tim+github.com@coote.org>
2019-10-29 13:52:46 +0000

    revert to stretch, using current anthropos .debs

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap buster "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	bootstrap stretch "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
 fi
